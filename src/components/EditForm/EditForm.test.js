28a9339d04dc46de256687ae273a2a2931412ce1
Magdalena Painter <paintermagdalena@gmail.com>
2020-07-07 20:28:44 -0400

    all tests passing

--- src/components/EditForm/EditForm.test.js
+++ src/components/EditForm/EditForm.test.js
@@ -5,6 +5,6 @@ import EditForm from './EditForm';
 
 it('renders the edit form page', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<BrowserRouter><EditForm /></BrowserRouter>, div);
+    ReactDOM.render(<BrowserRouter><EditForm match={{params: {id: 1}}}/></BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
--- src/components/Meal/Meal.test.js
+++ src/components/Meal/Meal.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Meal from './Meal';
 
 it('renders the meal page', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<MemoryRouter initialEntries={['/home']}><Meal /></MemoryRouter>, div);
+    ReactDOM.render(<BrowserRouter><Meal match={{params: {id: 1}}} /></BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
